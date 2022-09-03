3b5279ef2dfa8617901f4460a0aca4541449c6b4
leyla tunc <74448543+Leyla-T7@users.noreply.github.com>
2022-09-03 23:05:52 +0300

    Add files via upload

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,12 +1,8 @@
-######################################################
-# Diabetes Prediction with Logistic Regression
-######################################################
+########## DİABETES PREDİCTİONS WİTH LOGİSTİC REGRESSİON ###########
 
-# İş Problemi:
-
-# Özellikleri belirtildiğinde kişilerin diyabet hastası olup
-# olmadıklarını tahmin edebilecek bir makine öğrenmesi
-# modeli geliştirebilir misiniz?
+# İS PROBLEMI
+ # ozellikleri belirtilen kisilerin diyabet hastası olup olmadıklarını tahmın edebılecek
+ # bir makine ogrenmesı modelı gelıstırebılır mısınız?
 
 # Veri seti ABD'deki Ulusal Diyabet-Sindirim-Böbrek Hastalıkları Enstitüleri'nde tutulan büyük veri setinin
 # parçasıdır. ABD'deki Arizona Eyaleti'nin en büyük 5. şehri olan Phoenix şehrinde yaşayan 21 yaş ve üzerinde olan
@@ -14,10 +10,9 @@
 # bağımsız değişkenden oluşmaktadır. Hedef değişken "outcome" olarak belirtilmiş olup; 1 diyabet test sonucunun
 # pozitif oluşunu, 0 ise negatif oluşunu belirtmektedir.
 
-# Değişkenler
 # Pregnancies: Hamilelik sayısı
-# Glucose: Glikoz.
-# BloodPressure: Kan basıncı.
+# Glucose: Glikoz
+# BloodPressure: Kan basıncı (Diastolic(Küçük Tansiyon))
 # SkinThickness: Cilt Kalınlığı
 # Insulin: İnsülin.
 # BMI: Beden kitle indeksi.
@@ -25,25 +20,30 @@
 # Age: Yaş (yıl)
 # Outcome: Kişinin diyabet olup olmadığı bilgisi. Hastalığa sahip (1) ya da değil (0)
 
+# 1. exploratory data analysis
+# 2. data preprocessing
+# 3. model & prediction
+# 4. model evulation
+# 5. model validations:holdout
+# 6. model validations : 10 ford cross validations
+# 7. predictions for a  new observations
 
-# 1. Exploratory Data Analysis
-# 2. Data Preprocessing
-# 3. Model & Prediction
-# 4. Model Evaluation
-# 5. Model Validation: Holdout
-# 6. Model Validation: 10-Fold Cross Validation
-# 7. Prediction for A New Observation
 
-
-import matplotlib.pyplot as plt
+# Gerekli Kütüphane ve Fonksiyonlar
 import numpy as np
 import pandas as pd
 import seaborn as sns
-
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import RobustScaler
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix, classification_report, plot_roc_curve
-from sklearn.model_selection import train_test_split, cross_validate
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, plot_roc_curve, roc_auc_score
+import warnings
+warnings.simplefilter(action="ignore")
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
 
 def outlier_thresholds(dataframe, col_name, q1=0.05, q3=0.95):
     quartile1 = dataframe[col_name].quantile(q1)
@@ -53,6 +53,7 @@ def outlier_thresholds(dataframe, col_name, q1=0.05, q3=0.95):
     low_limit = quartile1 - 1.5 * interquantile_range
     return low_limit, up_limit
 
+
 def check_outlier(dataframe, col_name):
     low_limit, up_limit = outlier_thresholds(dataframe, col_name)
     if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
@@ -60,88 +61,55 @@ def check_outlier(dataframe, col_name):
     else:
         return False
 
-def replace_with_thresholds(dataframe, variable):
-    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+
+def replace_with_thresholds(dataframe, variable, q1=0.05, q3=0.95):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable, q1=0.05, q3=0.95)
     dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
     dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
 
+df= pd.read_csv("datasets/diabetes.csv")
+df.head()
+df.shape
 
-pd.set_option('display.max_columns', None)
-pd.set_option('display.float_format', lambda x: '%.3f' % x)
-pd.set_option('display.width', 500)
-
-
-
-######################################################
-# Exploratory Data Analysis
-######################################################
-
-df = pd.read_csv("datasets/diabetes.csv")
-
-##########################
-# Target'ın Analizi
-##########################
-
+# target analizi
 df["Outcome"].value_counts()
 
 sns.countplot(x="Outcome", data=df)
 plt.show()
 
-100 * df["Outcome"].value_counts() / len(df)
+df["Outcome"].value_counts() * 100 / len(df) #yuzde olarak gormke ıcın
 
-##########################
-# Feature'ların Analizi
-##########################
+df.describe().T
 
-df.head()
-
-df["BloodPressure"].hist(bins=20)
-plt.xlabel("BloodPressure")
+df["Glucose"].hist(bins=20)
+plt.xlabel("Glucose")
 plt.show()
 
-def plot_numerical_col(dataframe, numerical_col):
+def plot_numerical_col(dataframe,numerical_col):
     dataframe[numerical_col].hist(bins=20)
     plt.xlabel(numerical_col)
     plt.show(block=True)
-
-
+for col in df.columns:
+    plot_numerical_col(df, col)
+# outcome harıcı olanları ıstedık
+cols=[col for col in df.columns if "Outcome" not in col]
 for col in df.columns:
     plot_numerical_col(df, col)
 
-cols = [col for col in df.columns if "Outcome" not in col]
+# target and features
+df.groupby("Outcome").agg({"Pregnancies":"mean"})
 
+def target_summary_with_num(dataframe,target,numerical_num):
+    print(dataframe.groupby(target).agg({numerical_num: "mean"}), end="\n\n\n")
 
-# for col in cols:
-#     plot_numerical_col(df, col)
-
-df.describe().T
-
-##########################
-# Target vs Features
-##########################
-
-df.groupby("Outcome").agg({"Pregnancies": "mean"})
-
-def target_summary_with_num(dataframe, target, numerical_col):
-    print(dataframe.groupby(target).agg({numerical_col: "mean"}), end="\n\n\n")
-
-for col in cols:
+for col in df.columns:
     target_summary_with_num(df, "Outcome", col)
 
-
-
-######################################################
-# Data Preprocessing (Veri Ön İşleme)
-######################################################
-df.shape
-df.head()
-
 df.isnull().sum()
-
 df.describe().T
 
 for col in cols:
-    print(col, check_outlier(df, col))
+    print(col, check_outlier(df, col)) # her columnda aykırı deger varmı dıye bakyoruz
 
 replace_with_thresholds(df, "Insulin")
 
@@ -150,200 +118,75 @@ for col in cols:
 
 df.head()
 
-
-######################################################
-# Model & Prediction
-######################################################
+# MODEL AND PREDİCTİONS
 
 y = df["Outcome"]
-
 X = df.drop(["Outcome"], axis=1)
 
-log_model = LogisticRegression().fit(X, y)
-
+log_model = LogisticRegression().fit(X, y) # model kuruldu
 log_model.intercept_
-log_model.coef_
-
-y_pred = log_model.predict(X)
-
-y_pred[0:10]
-
-y[0:10]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-######################################################
-# Model & Prediction
-######################################################
-
-y = df["Outcome"]
-
-X = df.drop(["Outcome"], axis=1)
-
-log_model = LogisticRegression().fit(X, y)
-log_model.intercept_
+# [-1.23439588] bu deger b yanı bıas degerı
 log_model.coef_
 
 y_pred = log_model.predict(X)
 y_pred[0:10]
-
 y[0:10]
 
-
-
-
-
-
-
-
-######################################################
-# Model Evaluation
-######################################################
-
-def plot_confusion_matrix(y, y_pred):
-    acc = round(accuracy_score(y, y_pred), 2)
+# MODEL EVULATION
+# karmasıklık matrısı gosterımı ıcın  bır fonksıyon
+def plot_confusion_matrix(y,y_pred):
+    acc= round(accuracy_score(y, y_pred), 2)
     cm = confusion_matrix(y, y_pred)
     sns.heatmap(cm, annot=True, fmt=".0f")
-    plt.xlabel('y_pred')
-    plt.ylabel('y')
-    plt.title('Accuracy Score: {0}'.format(acc), size=10)
+    plt.xlabel("y_pred")
+    plt.ylabel("y")
+    plt.title("accuracy score: {0}".format(acc), size=10)
     plt.show()
 
-plot_confusion_matrix(y, y_pred)
+plot_confusion_matrix(y,y_pred)
 
-print(classification_report(y, y_pred))
+print(classification_report(y, y_pred)) # cıktıdakı 1 degerne bakarız 1 degerıne bakarak yorum yapabılrız
 
+# accuracy:0.78
+# precision:0.74
+# recall:0.58
+# f1 score:0.65
 
-# Accuracy: 0.78
-# Precision: 0.74
-# Recall: 0.58
-# F1-score: 0.65
-
-# ROC AUC
+# roc egrısınq gore yapalım
 y_prob = log_model.predict_proba(X)[:, 1]
 roc_auc_score(y, y_prob)
-# 0.83939
+# 0.8393955223880598
 
 
-######################################################
-# Model Validation: Holdout
-######################################################
+# MODEL DOGRULAMA  MODEL VALIDATİON :HOLDOUT
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=17)
 
-X_train, X_test, y_train, y_test = train_test_split(X,
-                                                    y,
-                                                    test_size=0.20, random_state=17)
-
-log_model = LogisticRegression().fit(X_train, y_train)
-
-y_pred = log_model.predict(X_test)
-y_prob = log_model.predict_proba(X_test)[:, 1]
-
-print(classification_report(y_test, y_pred))
-
-# Accuracy: 0.78
-# Precision: 0.74
-# Recall: 0.58
-# F1-score: 0.65
-
-# Accuracy: 0.77
-# Precision: 0.79
-# Recall: 0.53
-# F1-score: 0.63
-
-plot_roc_curve(log_model, X_test, y_test)
-plt.title('ROC Curve')
-plt.plot([0, 1], [0, 1], 'r--')
-plt.show()
-
-# AUC
-roc_auc_score(y_test, y_prob)
+log_model= LogisticRegression().fit(X_train,y_train)
+y_pred=log_model.predict(X_test)
+y_prob= log_model.predict_proba(X_test)[:,1]
+print(classification_report(y_test,y_pred))
 
 
-######################################################
-# Model Validation: 10-Fold Cross Validation
-######################################################
-
-y = df["Outcome"]
-X = df.drop(["Outcome"], axis=1)
-
-log_model = LogisticRegression().fit(X, y)
-
+## 10 KATLI CAPRAZ DOGRULAMA CROSS VALIDATION
+y= df["Outcome"]
+X= df.drop(["Outcome"], axis=1)
+log_model = LogisticRegression().fit(X, y) # az sayıda verımız varsa tum verı setını kullanarak da cross val yapabılırız
 cv_results = cross_validate(log_model,
-                            X, y,
-                            cv=5,
-                            scoring=["accuracy", "precision", "recall", "f1", "roc_auc"])
-
-
-
-# Accuracy: 0.78
-# Precision: 0.74
-# Recall: 0.58
-# F1-score: 0.65
-
-# Accuracy: 0.77
-# Precision: 0.79
-# Recall: 0.53
-# F1-score: 0.63
-
-
-cv_results['test_accuracy'].mean()
-# Accuracy: 0.7721
+                           X,y,
+                           cv=5,
+                           scoring=["accuracy","precision","recall","f1","roc_auc"])
+cv_results["test_accuracy"].mean()
 
 cv_results['test_precision'].mean()
-# Precision: 0.7192
 
 cv_results['test_recall'].mean()
-# Recall: 0.5747
 
 cv_results['test_f1'].mean()
-# F1-score: 0.6371
 
 cv_results['test_roc_auc'].mean()
-# AUC: 0.8327
-
-######################################################
-# Prediction for A New Observation
-######################################################
-
-X.columns
 
 random_user = X.sample(1, random_state=45)
 log_model.predict(random_user)
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
